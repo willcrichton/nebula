5d74b2c89f20ee4b95d4cf9d303fed96163b1876
Will Crichton <crichton.will@gmail.com>
2012-01-16 10:55:50 -0600

    New worker to do basic calculations (e.g. primes)

--- worker.js
+++ worker.js
@@ -1,5 +1,36 @@
 function handleMessage(event) {
-    var msg = event.data;
-    this.postMessage("Hello from a Web Worker!");
+    var data = event.data;
+	switch(data.calc){
+		case 'primes':
+			var numPrimes = data.data[0];
+			var primes = [2];
+			
+			var p = 3;
+			while( primes.length < numPrimes ){
+				isPrime = false;
+				for( var i = 3; i <= Math.sqrt(p); i += 2 ) {
+					if( p%i == 0 ) {
+						isPrime = true;
+						break;
+					}
+				}
+				
+				if( !isPrime ) primes.push( p );
+				p += 2;
+			}
+			
+			postMessage( primes.join(", ") );
+			break;
+			
+		case 'sum':
+			var sum = 0;
+			for( i in data.data )
+				sum += data.data[i];
+				
+			postMessage( sum );
+			break;
+	}
+		
 }
-this.addEventListener('message', handleMessage, false);
+addEventListener('message', handleMessage, false);
+
