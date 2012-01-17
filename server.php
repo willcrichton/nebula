98eff82fce7c19814f069f1747be1e9004dff53d
Will Crichton <crichton.will@gmail.com>
2012-01-16 21:51:20 -0600

    Optimized prime calc for multiple connections

--- server.php
+++ server.php
@@ -11,7 +11,14 @@ class ServerControl extends WebSocket {
 		$words = explode(" ", $msg);
 		switch($words[0]):
 			case "find_primes":
-				foreach($this->users as $u){
+				$max = intval($words[1]);
+				$i = 1;
+				$this->timer = microtime(true);
+				$output = exec("echo $max | primes.exe");
+				$endtime = microtime(true) - $this->timer;
+				$user->send("Primes calc after $endtime seconds");
+				
+				/*foreach($this->users as $u){
 					if(!$u->hasWorker){
 						$u->send("COMMAND create");
 						$u->hasWorker = true;
@@ -19,14 +26,18 @@ class ServerControl extends WebSocket {
 					
 					$this->timer = microtime(true);
 					$u->send("COMMAND newcalc");
-					$u->send($words[1]);
+					$u->send($i == 1 ? 3 : $max / count($this->users) * ($i - 1));
+					$u->send($max / count($this->users) * $i);
 					$u->send("COMMAND docalc primes");
-				}
+					
+					$i++;
+				}*/
 				
-				$this->timer2 = microtime(true);
+				/*$this->timer2 = microtime(true);
 				$primes = array();
 				$p = 3;
-				while( count($primes) < intval($words[1]) ){
+				$user->send("PHP doing prime calc of max " . $words[1]);
+				while( $p <= intval($words[1]) ){
 					$isPrime = false;
 					for( $i = 3; $i < sqrt($p); $i += 2 ){
 						if( $p%$i == 0 ){
@@ -37,7 +48,7 @@ class ServerControl extends WebSocket {
 					if(!$isPrime) $primes[] = $p;
 					$p += 2;
 				}
-				$user->send("PHP calc primes after " . (microtime(true) - $this->timer2) . " seconds");
+				$user->send("PHP calc primes after " . (microtime(true) - $this->timer2) . " seconds");*/
 				break;
 				
 			case "sum":
@@ -56,7 +67,7 @@ class ServerControl extends WebSocket {
 				break;
 				
 			default:
-				$user->send("length " . strlen($msg) . " after " . (microtime(true) - $this->timer) . " seconds, saying: \"$msg\"" );
+				$user->send("Chunk of length " . strlen($msg) . " received after " . (microtime(true) - $this->timer) . " seconds" );
 				break;
 		endswitch;
 	}
@@ -67,6 +78,6 @@ class ServerControl extends WebSocket {
 
 }
 
-$master = new ServerControl("localhost",8866);
+$master = new ServerControl("localhost",8849);
 
 ?>
